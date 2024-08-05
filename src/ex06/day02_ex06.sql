bdd04af97e1dcc32be2c5c3cc7d130a345ad5b2c
Анастасия <citadeln@mail.ru>
2024-08-05 14:46:32 +0700

    2 with

--- src/ex06/day02_ex06.sql
+++ src/ex06/day02_ex06.sql
@@ -1,20 +1,17 @@
-WITH ALL_PIZZAS AS
+WITH 
+ALL_PIZZAS AS
     (SELECT pizza_name,
         pizzeria.name AS pizzeria_name,
-        person_id
+        menu.id AS menu_id
     FROM menu
     INNER JOIN pizzeria ON pizzeria_id = pizzeria.id
-    ORDER BY pizza_name, pizzeria_name)
+    ORDER BY pizza_name, pizzeria_name),
+ALL_ORDERS AS
+    (SELECT menu_id
+    FROM person_order
+    INNER JOIN person ON person_order.person_id = person.id
+    WHERE person.name in ('Denis', 'Anna'))
 SELECT pizza_name,
        pizzeria_name
-INNER JOIN person ON person.id = person_id
 FROM ALL_PIZZAS
-
-
--- WITH orders_Denis_Anna AS
---     (SELECT missing_date::date
---     FROM generate_series('2022-01-01', '2022-01-10', interval '1 day') as missing_date
---     LEFT JOIN (select * FROM person_visits WHERE person_visits.person_id = 1 OR person_visits.person_id = 2) as person_visits2 ON visit_date = missing_date
---     WHERE person_visits2.visit_date is NULL)
--- SELECT * FROM orders_Denis_Anna
--- ORDER BY pizza_name, pizzeria_name;
+INNER JOIN ALL_ORDERS ON ALL_PIZZAS.menu_id = ALL_ORDERS.menu_id
